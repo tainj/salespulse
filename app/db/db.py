8715fa203e977b2275007305d588f838e3cc2db0
Anton <leshoyvantoha@yandex.ru>
2025-10-12 01:17:27 +0300

    new path db

--- app/db/db.py
+++ app/db/db.py
@@ -1,13 +1,21 @@
+import os
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.models import Base  # импортируем модели
-import logging
+from app.models import Base  # твои модели
 
-DATABASE_URL = "sqlite:///db/store.db"
+# Указываем абсолютный путь к БД — внутри твоей домашней директории
+DB_PATH = "/home/tainj/salespulse/db/store.db"
+
+# Создаём папку db, если её нет
+os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+
+# Формируем URL с абсолютным путём
+DATABASE_URL = f"sqlite:///{DB_PATH}"
+
 logger = logging.getLogger(__name__)
 
 engine = create_engine(DATABASE_URL, echo=True)
-
 SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
